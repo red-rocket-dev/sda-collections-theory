4e99993606e0c131c738b2a0574257e76cfc3dc4
Jakub <kubplon@gmail.com>
2020-03-07 00:55:19 +0100

    List of topics to cover

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,11 @@
+package pl.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        //List - 2 rodzaje, wewnetrzna struktura + własna implementacja
+        //Set
+        //Map
+        //sortowanie
+        //iterowanie for each, iterator, get - dlaczego nie uzywac, porownanie do tablicy
+    }
+}
